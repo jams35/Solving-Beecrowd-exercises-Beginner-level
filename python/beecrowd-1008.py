5a232bc2be716fb12cca9d417208a5a5ab5ffa3e
Augusto Maia de Souza <joao.augusto.ms@outlook.com>
2022-12-12 00:38:43 -0400

    Resolução do exercício 1008

---
+++ python/beecrowd-1008.py
@@ -0,0 +1,7 @@
+numberEmployees = int(input(''))
+workedHours = int(input(''))
+amountHour = float(input(''))
+
+calc = amountHour * workedHours
+
+print('NUMBER = {}\nSALARY = U$ {:.2f}'.format(numberEmployees, calc))
