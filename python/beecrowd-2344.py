7db53b3e5bac5577bd9e7c85060f3427218c35b6
Augusto Maia de Souza <joao.augusto.ms@outlook.com>
2022-12-22 16:58:23 -0400

    Resolução do exercício 2344

---
+++ python/beecrowd-2344.py
@@ -0,0 +1,21 @@
+n = int(input(''))
+
+if n == 0:
+    print('E')
+
+if n >= 1 and n <=35:
+    print('D')
+
+if n > 35 and n <=60:
+    print('C')
+
+if n > 60 and n <=85:
+    print('B')
+    
+if n > 85 and n <= 100:
+    print('A')
+
+    
+    
+
+        
